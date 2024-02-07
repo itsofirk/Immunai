a5df9b766fcb81741534af634ff44cea337ab9f2
Tetro <itsofirk@gmail.com>
2024-02-07 19:53:38 +0200

    initialized a logger

--- src/common/__init__.py
+++ src/common/__init__.py
@@ -1,4 +1,12 @@
+import logging
 from .config import Settings
 
 
 settings = Settings()
+logger = logging.getLogger()
+
+fmt = logging.Formatter("[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s")
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(fmt)
+
+logger.addHandler(console_handler)
