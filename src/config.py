0eed4ced219d8d31afb84b7d038b74e05bdff1cb
Tetro <itsofirk@gmail.com>
2024-02-07 17:38:14 +0200

    created config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    raw_data_dir: str = 'resources/raw_experiment_data/'
+    processed_data_dir: str = 'resources/processed_data/'
+    validated_data_dir: str = 'resources/validated_data/'
