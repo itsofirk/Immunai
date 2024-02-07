28ac9209b3a7949b14ff8f1d18fe7bc5669a7107
Tetro <itsofirk@gmail.com>
2024-02-07 18:09:23 +0200

    added bronze.py - v1

---
+++ src/bronze.py
@@ -0,0 +1,25 @@
+from filesystem import get_raw_data_path, get_processed_data_path, load_json, dump_json
+
+
+def process_experiment(experiment_id):
+    input_file_path = get_raw_data_path(experiment_id)
+    raw_data = load_json(input_file_path)
+
+    necessary_data = []
+
+    for entry in raw_data:
+        if entry["environment"]["name"] == "In vivo":
+            relevant_data = {
+                "cell_type": entry["cell_type"]["name"],
+                "cell_response": entry["cell_response"]
+            }
+
+            necessary_data.append(relevant_data)
+
+    output_file_path = get_processed_data_path(experiment_id)
+    dump_json(output_file_path, necessary_data)
+
+
+if __name__ == "__main__":
+    _experiment_id = 'EXP_001'
+    process_experiment(_experiment_id)
