e3e4c105d6021dc41d50446e6463c4af1b50d705
Tetro <itsofirk@gmail.com>
2024-02-07 18:31:13 +0200

    extracted methods from process_experiment

--- src/bronze.py
+++ src/bronze.py
@@ -1,20 +1,24 @@
 from common.filesystem import get_raw_data_path, get_clean_data_path, load_json, dump_json
 
+IN_VIVO = "In vivo"
+
+
+def is_relevant(entry):
+    return entry["environment"]["name"] == IN_VIVO
+
+
+def extract_relevant_fields(entry):
+    return {
+        "cell_type": entry["cell_type"]["name"],
+        "cell_response": entry["cell_response"]
+    }
+
 
 def process_experiment(experiment_id):
     input_file_path = get_raw_data_path(experiment_id)
     raw_data = load_json(input_file_path)
 
-    necessary_data = []
-
-    for entry in raw_data:
-        if entry["environment"]["name"] == "In vivo":
-            relevant_data = {
-                "cell_type": entry["cell_type"]["name"],
-                "cell_response": entry["cell_response"]
-            }
-
-            necessary_data.append(relevant_data)
+    necessary_data = [extract_relevant_fields(entry) for entry in raw_data if is_relevant(entry)]
 
     output_file_path = get_clean_data_path(experiment_id)
     dump_json(output_file_path, necessary_data)
