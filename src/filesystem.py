8143bdd3657770100f11a8491529ad4dbb27b930
Tetro <itsofirk@gmail.com>
2024-02-07 17:44:47 +0200

    added get_raw_data to mask the json library

--- src/filesystem.py
+++ src/filesystem.py
@@ -1,4 +1,5 @@
 import os
+import json
 from config import Settings
 
 settings = Settings()
@@ -15,3 +16,7 @@ def get_processed_data_path(experiment_id):
 def get_validated_data_path(experiment_id):
     return os.path.join(settings.validated_data_dir, f'{experiment_id}.json')
 
+
+def get_raw_data(experiment_id):
+    with open(get_raw_data_path(experiment_id), 'r') as input_file:
+        return json.load(input_file)
