00c022856a4e939153c2b1e0c669ee582e468e14
Tetro <itsofirk@gmail.com>
2024-02-07 18:09:04 +0200

    added a leading "../" for the resources path fields renamed get_raw_data to load_json added dump_json to filesystem.py

--- src/filesystem.py
+++ src/filesystem.py
@@ -18,9 +18,15 @@ def get_validated_data_path(experiment_id):
     return os.path.join(settings.validated_data_dir, f'{experiment_id}.json')
 
 
-def get_raw_data(experiment_id):
+def load_json(json_file_path):
     try:
-        with open(get_raw_data_path(experiment_id), 'r') as input_file:
+        with open(json_file_path, 'r') as input_file:
             return json.load(input_file)
     except FileNotFoundError:
         raise ExperimentNotFoundError("File not found. Please check the experiment ID and try again.")
+
+
+def dump_json(json_file_path, data):
+    os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+    with open(json_file_path, 'w') as output_file:
+        json.dump(data, output_file)
